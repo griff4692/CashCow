727ccaa6999eb025360d6a24e9d58fbd7261ed7b
Griffin Adams <griff4692@gmail.com>
2015-07-31 15:34:48 -0400

    reseeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
 fnames = ["Bertha", "Patty", "Seamus", "Angelika", "Nelson", "Pablo", "Quincy", "Carmelo", "Peter", "Pam" "Kristaps"]
 lnames = ["Hewes", "Valentine", "Heaney", "Houston", "Jones", "Mandela", "Porzingis", "Anthony", "Pan", "DeLaPieu"]
 
-25.times do |i|
+projects = {
+	"Art" => [
+		["thistitle", "description"],
+		["heytitle", "description"],
+		["tityoule","description"],
+		["tikn,tle","description"]
+	],
+	"Music" => [
+		["titoile", "description"],
+		["titmnle", "description"],
+		["titwwxle","description"],
+		["titmnle","description"]
+	],
+	"Philanthropy" => [
+		["titile", "description"],
+		["titklne", "description"],
+		["tikljmtle","description"],
+		["tijfwtle","description"]
+	],
+}
+
+images = {
+	"Art" => 'http://i.imgur.com/WrobxaJ.jpg',
+	"Philanthropy" => 'http://livingcivil.com/wp-content/uploads/2011/10/Story4.jpg',
+	"Music" => 'http://8020.photos.jpgmag.com/3510083_206038_a3b8fe42df_p.jpg'
+}
+
+5.times do |i|
 	user = User.create!(
 		fname: fnames.sample,
 		lname: lnames.sample,
@@ -13,17 +40,18 @@ lnames = ["Hewes", "Valentine", "Heaney", "Houston", "Jones", "Mandela", "Porzin
 	randYear = (2016..2020).to_a
 	randMonth = (1..12).to_a
 
-	rand(5).times do |j|
+	6.times do |j|
 		category = categories.sample
+		project = projects[category].sample
 
 		Project.create!(
 			user_id: user.id,
-			title:  Faker::Team.name + i.to_s + j.to_s,
-			description: Faker::Lorem.paragraph,
+			title: "title" + i.to_s + j.to_s,
+			description: project[1],
 			category: category,
 			goal: rand(2500000),
 			deadline: Date.new(randYear.sample, randMonth.sample, 1),
-			image: Faker::Avatar.image
+			image: images[category]
 		)
 	end
 end
